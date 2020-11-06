a12e73ae4dc664a8a0be82d9cbd938a67b0a2a75
kacou48 <63119982+kacou48@users.noreply.github.com>
2020-11-06 15:42:48 -0800

    Add files via upload

--- send_mail_app/admin.py
+++ send_mail_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Contact
+from . models import Contact
 
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('nom', 'email', 'message', 'date')
