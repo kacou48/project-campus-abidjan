a73e298f6512370028a029c4a6b0b2324b7f3eb3
kacou48 <63119982+kacou48@users.noreply.github.com>
2020-11-07 00:01:53 -0800

    Add files via upload

--- enmain/settings.py
+++ enmain/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 #static files (css, Images JS)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
 os.path.join(BASE_DIR, 'static'),
